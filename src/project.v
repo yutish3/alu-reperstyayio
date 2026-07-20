949a4d1a982dc5f5a90be506ca5a4045f6a0e973
yutish kumar <110885523+yutish3@users.noreply.github.com>
2026-07-20 21:33:16 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,29 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module n_bit_alu #(parameter  n = 19 ) (
+    input wire [n-1 : 0] a,
+    input wire [n-1 : 0] b,
+    input wire [2:0] alu_control, //0,1,2 are 3 bits so we can have 2³ operations ie 8
+    output reg [n-1 : 0] result,
+    output wire zero //flag output for checking zero/2 numbers being equal
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+always @(*) begin
+    case (alu_control)
+    3'b000: result = a + b;  //if input wire is 000 it adds
+    3'b001: result = a - b;  //if input wire is 000 it subtracts
+    3'b010: result = a & b;  //if input wire is 000 it performs bitwise of AND gate
+    3'b011: result = a | b;  //if input wire is 000 it performs bitwise of OR gate
+    3'b100: result = a ^ b;  //if input wire is 000 it performs bitwise of XOR gate
+    3'b101: result = a << 1; //if input wire is 101 it shifts input a to the left by 1 position (multiplying by 2)
+    3'b110: result = a >> 1; //if input wire is 110 it shifts input a to the right by 1 position (divides by 2)
+    default: result = 0; //sets result to 0 in case of unexpected code
+    endcase
+end
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+assign zero = (result == 0) ? 1'b1 : 1'b0; //it checks if both are equal or not
 
 endmodule
+
+
+/*this one is simple except the 101 and 110 so how it works is 
+ex number is 14 so in 5 bits it will be 01110 ie 8+4+2 if we move them left by 1 it will be 11100 16+8+4 = 28 twice of 14
+same goes for dividing*/
